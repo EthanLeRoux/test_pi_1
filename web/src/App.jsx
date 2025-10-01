31979bfbab6cf636a9d7f22b03e3a97deed288ff
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-10-01 16:13:23 +0200

    it

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   useEffect(() => {
     const fetchMotions = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/motion/all') // replace with your API URL
+        const response = await fetch('http://localhost:5000/api/motion/all')
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`)
         }
